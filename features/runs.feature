bb1a049e45ebd261003ac1cf6610cccbe790fe97
finbar sheahan <finbar.sheahan@wearefriday.com>
2016-01-08 17:06:46 +0000

    testing there is a test on a test page

--- features/runs.feature
+++ features/runs.feature
@@ -1,5 +1,12 @@
 Feature: Tests for runs
-  Scenario: showing the run
-    Given there is a run
+  Background:
+    Given there is a run with a test
     When we visit the runs page
+
+  @javascript
+  Scenario: showing the run
     Then we should see the run
+
+  @javascript
+  Scenario: showing a test on the run page
+    Then we should see the test
--- features/step_definitions/runs_steps.rb
+++ features/step_definitions/runs_steps.rb
@@ -1,7 +1,18 @@
-Given(/^there is a run$/) do
+Given(/^there is a run with a test$/) do
   project = Project.create!(name: 'testproject')
   suite = project.reload.suites.create!(name: 'testsuite')
   suite.runs.create
+  RestClient.post(
+        "#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/tests",
+        test: {
+          run_id: 1,
+          name: 'test test',
+          platform: 'osx',
+          browser: 'poltergiest',
+          width: '1024',
+          screenshot: File.open('features/support/screenshots/run1.png')
+        }
+      )
 end
 
 When(/^we visit the runs page$/) do
@@ -12,4 +23,8 @@ Then(/^we should see the run$/) do
   within('h1') do
     expect(page).to have_content('Run #1')
   end
+end
+
+Then(/^we should see the test$/) do
+  expect(page).to have_css('#test_1')
 end
