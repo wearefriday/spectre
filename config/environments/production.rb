b5d64062aa57d33572286030574cafb6079c61c0
Lindsey Johnson <LindseyJJ@users.noreply.github.com>
2020-02-06 13:27:55 -0700

    uncommented lines to match other production apps and added line to debug logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -79,6 +79,7 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
+    ActiveSupport::Logger.logger_outputs_to?(logger, STDOUT)
   end
 
   # Do not dump schema after migrations.
