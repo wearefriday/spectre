518a77c549fdb78b2533d9246b44f55a75d43f0a
Lindsey Johnson <LindseyJJ@users.noreply.github.com>
2020-03-19 14:30:26 -0600

    took out create method and oauth methods

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,12 +4,6 @@ require 'image_processor'
 
 class TestsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  skip_before_action :oauth_authenticate!, only: [:create]
-
-  http_basic_authenticate_with(
-    name: 'spectre_api',
-    password: ENV['API_PASSWORD'] || SecureRandom.uuid
-  )
 
   def new
     @test = Test.new
@@ -22,20 +16,5 @@ class TestsController < ApplicationController
       @test.save
       redirect_to project_suite_run_url(@test.run.suite.project, @test.run.suite, @test.run)
     end
-  end
-
-  def create
-    if test_params[:crop_area]
-      ImageProcessor.crop(test_params[:screenshot].path, test_params[:crop_area])
-    end
-    @test = Test.create!(test_params)
-    ScreenshotComparison.new(@test, test_params[:screenshot])
-    render json: @test.to_json
-  end
-
-  private
-
-  def test_params
-    params.require(:test).permit(:name, :browser, :size, :screenshot, :run_id, :source_url, :fuzz_level, :highlight_colour, :crop_area)
   end
 end
