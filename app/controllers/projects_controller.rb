a48070d1a46d1db9434b184654413549872ff47d
Lindsey Johnson <LindseyJJ@users.noreply.github.com>
2020-03-25 16:14:05 -0600

    added delete project table tag and are you sure message

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,10 +6,7 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    respond_to do |format|
-      @project = Project.find_by_slug!(params[:slug]).destroy
-      format.js
-      format.html { redirect_to projects_path }
-    end
+    @project = Project.find_by_slug!(params[:slug]).destroy
+    redirect_to projects_path
   end
 end
